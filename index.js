714b0a42f37a6b4a4335a700c820506148412c16
+ <+>
2023-03-21 13:13:24 -0400

    Submit first-class-functions lab lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (array) => (array.slice(0,2));
+
+const returnLastTwoDrivers = (array) => (array.slice(2,4));
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num) {
+    return function(fare) {
+        return num * 5;
+    };
+};
+
+
+const fareDoubler = (num) => (num * 2)
+
+const fareTripler = (num) => (num * 3)
+
+function selectDifferentDrivers(drivers, fn) {
+    if (fn == returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(drivers);
+  } else {
+    return returnLastTwoDrivers(drivers);
+  }
+}
+
